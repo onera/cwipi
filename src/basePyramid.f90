78835f508129a214bd0c4b6dc2cf83c6cb6862f8
Christophe Peyret <christophe@peyret.fr>
2015-01-30 15:12:55 +0100

    pyramid calcul des points equidistants

--- src/basePyramid.f90
+++ src/basePyramid.f90
@@ -16,37 +16,57 @@ contains
     real(8), intent(out), pointer :: uvw(:,:)
     logical, intent(in)           :: display
     !---
-    integer                       :: i,j
+    integer                       :: iu,iv,iw
+    integer                       :: np,iNod,iOrd
     real(8)                       :: a
-    real(8), pointer              :: r1D(:)
+    real(8)                       :: r1D(0:ord)
+    !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+        
+    !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    np=0
+    do iu=1,ord+1
+      np=np+iu*iu
+    enddo
+    if( display )print '("np=",i6)',np
     !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     
     !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    allocate(r1D(0:ord))
+    allocate(uvw(3,np))
     !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     
     !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    do i=0,ord
+    iNod=0
+    do iw=0,ord
       
+      !> a : square [-a,+a]x[-a,+a] @ iw
       if( ord==0 )then
         a=0d0
       else
-        a=1d0-real(i,kind=8)/real(ord,kind=8) 
+        a=1d0-real(iw,kind=8)/real(ord,kind=8) 
       endif
       !if( display )print '("a=",f12.9)',a
       
+      !> r1d subdivision of [-a,+a]
       r1d(0)=-a
-      do j=0,ord-i-1
-        r1d(j)=-a+(2d0*a)*real(j,kind=8)/real(ord-i,kind=8)
+      do iu=1,ord-iw-1
+        r1d(iu)=-a+(2d0*a)*real(iu,kind=8)/real(ord-iw,kind=8)
       enddo
-      r1d(ord-i)=+a
+      r1d(ord-iw)=+a
+      !if( display )print '("r1d=",15(f12.9,1x))',r1d(0:ord-iw)
       
-      if( display )print '("r1d=",15(f12.9,1x))',r1d(0:ord-i)
+      
+      do iu=0,ord-iw
+        do iv=0,ord-iw
+          iNod=iNod+1
+          uvw(1:3,iNod)=[ r1d(iu), r1d(iv), real(iw,kind=8)/real(ord,kind=8)  ]
+          if( display )print '("uvw(",i6,")=",3(f12.9,1x))',iNod,uvw(1:3,iNod)
+        enddo
+      enddo  
+      
     enddo
     !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     
     !>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    deallocate(r1D)
     !<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     
 !    x = []; y = []; z= [];
