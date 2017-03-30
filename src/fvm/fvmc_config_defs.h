461b93334838365be23c29489ed0502a8ee64886
Eric Quemerais <eric.quemerais@onera.fr>
2017-03-30 13:43:02 +0200

    Switch to CMake - Compilation ok

--- src/fvm/fvmc_config_defs.h
+++ src/fvm/fvmc_config_defs.h
@@ -99,24 +99,19 @@ extern "C" {
 
 /* C99 _Bool type */
 
-#ifdef HAVE_STDBOOL_H
-# include <stdbool.h>
-#else
-# ifndef HAVE__BOOL
-#  ifdef __cplusplus
+#ifdef __cplusplus
 typedef bool _Bool;
-#  else
+#else
+# if HAVE_STDBOOL_H
+#  include <stdbool.h>
+# else
+#  if !HAVE__BOOL
 typedef unsigned char _Bool;
 #  endif
-# else
-#  ifdef __cplusplus
-typedef bool _Bool;
-#  endif 
 # endif
-/*# define bool _Bool*/
+# define bool _Bool
 # define false 0
 # define true 1
-# define __bool_true_false_are_defined 1
 #endif
 
 /* int32_t type */
