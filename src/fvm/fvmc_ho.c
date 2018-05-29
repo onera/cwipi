5395322c8c47055a340e50f91429204f8944d9a6
Eric Quemerais <eric.quemerais@onera.fr>
2018-05-29 11:07:24 +0200

    ho interpolate - add interpolate functions in fvmc_ho

--- src/fvm/fvmc_ho.c
+++ src/fvm/fvmc_ho.c
@@ -261,3 +261,109 @@ fvmc_ho_shape_on_cell_1d (const fvmc_element_t type,
   bftc_error(__FILE__, __LINE__, 0,
              _("fvmc_ho_shape_on_cell_1d : Not implement yet\n"));
 }
+
+
+/*----------------------------------------------------------------------------
+ * 
+ * Interpolate field to the target point_coords in a 3D cell
+ * 
+ * parameters:
+ *   type              <-- element type
+ *   order             <-- element order
+ *   ho_vertex_num     <-- high order vertex num (internal ordering)
+ *   local_to_user     <-- local to user ordering (for type)
+ *   vertex_coords     <-- vertex coordinates
+ *   point_coords      <-- point inside cell (or on boundary) 
+ *   shape             <-- barycenter's coordinates
+ *   stride_field      <-- field stride
+ *   source_field      <-- source field (user ordering) 
+ *   target_field      --> target field (defined to point_coords)
+ *
+ *----------------------------------------------------------------------------*/
+
+void 
+fvmc_ho_interp_in_cell_3d (const fvmc_element_t type,
+                           const int order,
+                           const int *ho_vertex_num,
+                           const int *local_to_user,
+                           const double *vertex_coords,
+                           const double *point_coords,
+                           const double *shape,
+                           const int stride_field,
+                           const double *src_field,
+                           double *target_field)
+                           
+{
+  bftc_error(__FILE__, __LINE__, 0,
+             _("fvmc_ho_interp_in_cell_3d : Not implement yet\n"));
+}
+
+
+/*----------------------------------------------------------------------------
+ * 
+ * Interpolate field to the target point_coords on a 2D cell
+ * 
+ * parameters:
+ *   type              <-- element type
+ *   order             <-- element order
+ *   ho_vertex_num     <-- high order vertex num (internal ordering)
+ *   local_to_user     <-- local to user ordering (for type)
+ *   vertex_coords     <-- vertex coordinates
+ *   point_coords      <-- point inside cell (or on boundary) 
+ *   shape             <-- barycenter's coordinates
+ *   stride_field      <-- field stride
+ *   source_field      <-- source field (user ordering) 
+ *   target_field      --> target field (defined to point_coords)
+ *
+ *----------------------------------------------------------------------------*/
+
+void 
+fvmc_ho_interp_on_cell_2d (const fvmc_element_t type,
+                           const int order,
+                           const int *ho_vertex_num,
+                           const int *local_to_user,
+                           const double *vertex_coords,
+                           const double *point_coords,
+                           const double *shape,
+                           const int stride_field,
+                           const double *src_field,
+                           double *target_field)
+{
+  bftc_error(__FILE__, __LINE__, 0,
+             _("fvmc_ho_interp_on_cell_2d : Not implement yet\n"));
+}
+
+
+/*----------------------------------------------------------------------------
+ * 
+ * Interpolate field to the target point_coords on a 2D cell
+ * 
+ * parameters:
+ *   type              <-- element type
+ *   order             <-- element order
+ *   ho_vertex_num     <-- high order vertex num (internal ordering)
+ *   local_to_user     <-- local to user ordering (for type)
+ *   vertex_coords     <-- vertex coordinates
+ *   point_coords      <-- point inside cell (or on boundary) 
+ *   shape             <-- barycenter's coordinates
+ *   stride_field      <-- field stride
+ *   source_field      <-- source field (user ordering) 
+ *   target_field      --> target field (defined to point_coords)
+ *
+ *----------------------------------------------------------------------------*/
+
+void 
+fvmc_ho_interp_on_cell_1d (const fvmc_element_t type,
+                           const int order,
+                           const int *ho_vertex_num,
+                           const int *local_to_user,
+                           const double *vertex_coords,
+                           const double *point_coords,
+                           const double *shape,
+                           const int stride_field,
+                           const double *src_field,
+                           double *target_field)
+{
+  bftc_error(__FILE__, __LINE__, 0,
+             _("fvmc_ho_shape_on_cell_1d : Not implement yet\n"));
+}
