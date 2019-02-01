a424c62fcdaf48fe0d349fe7a03a5f8bc6cf8a2f
Romain Casta <casta@nori.cerfacs.fr>
2019-02-01 13:46:21 +0100

    Temporary change to deal with negative face indexes.

--- paradigm/src/mesh/pdm_mesh_nodal.c
+++ paradigm/src/mesh/pdm_mesh_nodal.c
@@ -3276,7 +3276,7 @@ PDM_g_num_t      *numabs
           {
             PDM_l_num_t *cell_face_cell = cell_face_courant + cell_face_idx_courant[i] - adjust;
             for (int j = 0; j < cell_face_nb_courant[i]; j++) {
-              tag_face_poly3d[cell_face_cell[j] - 1] = 0;
+             // tag_face_poly3d[cell_face_cell[j] - 1] = 0;
             }
             *numabs_poly3d_courant = numabs_courant[i];
             numabs_poly3d_courant += 1;
