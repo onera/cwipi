47e91bed3a1d22310479ac1b4326ca46c3345676
Romain Casta <casta@cerfacs.fr>
2019-06-05 15:21:37 +0200

    PDM computation of barycentric coordinates of a set of points in 2D polygons.

--- paradigm/src/mesh/pdm_geom_elem.h
+++ paradigm/src/mesh/pdm_geom_elem.h
@@ -317,7 +317,34 @@ PDM_geom_elem_quad_properties
  int         *isDegenerated
 );
 
+/**
+ * \brief Compute the barycentric coordinates of a set of points inside
+          their belonging polygons. 
+ *  
+ *  @param [in]  nPoints               Number of points
+ *  @param [in]  ptsLocations          Numbering of the belonging polygons inside the connectivityIndex
+ *  @param [in]  connectivityIndex     Mesh connectivity Index
+ *  @param [in]  connectivity          Mesh connectivity
+ *  @param [in]  coords                Mesh coordinates
+ *  @param [out] barCoordsIndex        Pointer to the barycentric coordinates index              
+ *  @param [out] barCoordsIndex        Pointer to the barycentric coordinates             
+ * 
+ *  @return                     The status of properties computation convergence                
+ */
 
+int 
+PDM_geom_elem_compute_polygon_barycentric_coordinates
+(
+ const int           n_points,
+ const int          *pts_locations,
+ const double       *pts_coords,
+ const int          *connectivityIndex,
+ const int          *connectivity,
+ const double       *coords,
+ int               **barCoordsIndex,
+ double            **barCoords
+);               
+              
 /**
  *  \brief Polygon properties
  *
