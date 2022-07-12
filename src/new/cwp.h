b97e58ac6bf51cfdbfe72da6b069793aa48e4e9f
Eric Quemerais <eric.quemerais@onera.fr>
2022-07-12 20:19:23 +0200

    Add to the API get properties of blocks

--- src/new/cwp.h
+++ src/new/cwp.h
@@ -1329,6 +1329,31 @@ CWP_Mesh_interf_block_std_set
 
 
 /**
+ * \brief Get the properties of a standard block of the interface mesh.
+ *
+ * \param [in]  local_code_name  Local code name
+ * \param [in]  cpl_id           Coupling identifier
+ * \param [in]  i_part           Partition identifier
+ * \param [in]  block_id         Block identifier
+ * \param [out]  n_elts           Number of elements
+ * \param [out]  connec           Connectivity (size = n_vertex_elt * n_elts)
+ * \param [out]  global_num       Pointer to global element number (or NULL)
+ */
+
+void
+CWP_Mesh_interf_block_std_get
+(
+ const char        *local_code_name,
+ const char        *cpl_id,
+ const int          i_part,
+ const int          block_id,
+ int               *n_elts,
+ int              **connec,
+ CWP_g_num_t      **global_num
+);
+
+
+/**
  * \brief Set a generic high order block to the interface mesh. <b>(Not implemented yet)</b>
  *
  * \param [in]  local_code_name  Local code name
@@ -1353,6 +1378,34 @@ CWP_Mesh_interf_h_order_block_set
  const int          order,
  int                connec[],
  CWP_g_num_t        global_num[]
+);
+
+
+/**
+ * \brief Get the properties of a generic high order block of the interface mesh. <b>(Not implemented yet)</b>
+ *
+ * \param [in]  local_code_name  Local code name
+ * \param [in]  cpl_id           Coupling identifier
+ * \param [in]  i_part           Partition identifier
+ * \param [in]  block_id         Block identifier
+ * \param [out]  n_elts           Number of elements
+ * \param [out]  order            Element order
+ * \param [out]  connec           Connectivity (size = n_vertex_elt * n_elts)
+ * \param [out]  global_num       Pointer to global element number (or NULL)
+ *
+ */
+
+void
+CWP_Mesh_interf_h_order_block_get
+(
+ const char        *local_code_name,
+ const char        *cpl_id,
+ const int          i_part,
+ const int          block_id,
+ int               *n_elts,
+ int               *order,
+ int              **connec,
+ CWP_g_num_t      **global_num
 );
 
 
