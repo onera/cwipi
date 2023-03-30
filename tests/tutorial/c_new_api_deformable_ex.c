2b63d876ede433088fcd2ccea213fc6a2c258653
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2023-03-30 13:52:59 +0200

    [tutorials] set up deformable_ex

---
+++ tests/tutorial/c_new_api_deformable_ex.c
@@ -0,0 +1,248 @@
+/*
+  This file is part of the CWIPI library.
+
+  Copyright (C) 2011  ONERA
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <time.h>
+#include <math.h>
+
+#include "cwp.h"
+#include "pdm_error.h"
+#include "pdm_priv.h"
+#include "pdm_generate_mesh.h"
+
+/*----------------------------------------------------------------------
+ *
+ * Main : advanced test : Deformable (code 2)
+ *
+ *---------------------------------------------------------------------*/
+
+int
+main(int argc, char *argv[]) {
+
+  // Initialize MPI :
+  MPI_Init(&argc, &argv);
+  int i_rank;
+  int n_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &i_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &n_rank);
+
+  // Check running on correct number of MPI ranks :
+  int n_partition = 0;
+  while(2 * pow(n_partition, 2) < n_rank) n_partition++;
+
+  const int two = 2;
+  int n2 = two * (int) pow(n_partition, two);
+
+  if (n2 != n_rank) {
+    if (i_rank == 0)
+      printf("      Not executed : only available if the number of processus in the form of '2 * n^2' \n");
+    exit(1);
+    return EXIT_SUCCESS;
+  }
+
+  // Initialize CWIPI :
+  // Use CWP_Init for code2 written in C.
+  // ------------------------------------------------------- To fill in
+  int n_code = 1;
+  const char  **code_name  = malloc(sizeof(char *) * n_code);
+
+  code_name[0]      = "code2";
+
+  // ---------------------------------------------------- End To fill in
+
+  // Create the coupling :
+  // In this tutorial the mesh will be deformed over the coupling
+  // itreations. Those iterations mimic solver steps. Use CWP_Cpl_create
+  // to couple code1 with code2. A rectangular mesh is used with triangle
+  // elements. Operate the localization with the octree method.
+  // ------------------------------------------------------- To fill in
+  int n_part = 1;
+  const char  *coupling_name     = "coupling";
+  const char **coupled_code_name = malloc(sizeof(char *) * n_code);
+  coupled_code_name[0] = "code1";
+
+  // ---------------------------------------------------- End To fill in
+
+  // Set coupling visualisation:
+  // Use CWP_Visu_set to output ASCII Ensight format files
+  // at each iteration step. Open the CHR.case file in
+  // paraview to visualize the deformed mesh and exchanged fields.
+  // ------------------------------------------------------- To fill in
+
+  // ---------------------------------------------------- End To fill in
+
+  // Create mesh :
+  // A ParaDiGM library function is used to generate the rectangular mesh.
+  // PDM_MPI_mpi_2_pdm_mpi_comm is used to convert the MPI communicator into
+  // the expected ParaDiGM format. It is the users responsability to
+  // free arrays from _simplified mesh generation functions. In a real life
+  // coupling case here a user generated mesh would be read/loaded/given.
+  int     n_vtx = 0;
+  int     n_elt = 0;
+  double *coords      = NULL;
+  int    *elt_vtx_idx = NULL;
+  int    *elt_vtx     = NULL;
+  PDM_generate_mesh_rectangle_simplified(PDM_MPI_mpi_2_pdm_mpi_comm((void *) &intra_comm[0]),
+                                         &n_vtx,
+                                         &n_elt,
+                                         &coords,
+                                         &elt_vtx_idx,
+                                         &elt_vtx);
+
+  // Interations :
+  // At each iteration the mesh coordinates and the exchanged fields are modified.
+  const char *send_field_name = "chinchilla";
+  const char *recv_field_name = "girafe";
+  int         n_components    = 1;
+  double     *send_field_data = malloc(sizeof(double) * n_vtx);
+  double     *recv_field_data = malloc(sizeof(double) * n_vtx);
+
+  const int    itdeb = 1;
+  const int    itend = 50;
+  const double freq  = 0.20;
+  const double ampl  = 0.012;
+  const double phi   = 0.1;
+  double       ttime = 0.0;
+  double       dt    = 0.1;
+
+  double omega = 2.0*acos(-1.0)*freq;
+
+  for (int it = itdeb; it <= itend; it ++) {
+
+    ttime = (it-itdeb)*dt;
+
+    // Deform mesh and set send field :
+    // The field that will be sent, "chinchilla", is set to the value of
+    // the z-coordinate of the mesh nodes.
+    for (int i = 0; i < n_vtx; i++) {
+      coords[3 * i + 2]  = ampl * (coords[3 * i]*coords[3 * i]+coords[1 + 3 * i]*coords[1 + 3 * i])*cos(omega*ttime+phi);
+      send_field_data[i] = coords[3 * i + 2];
+    }
+
+    // First iteration :
+    // Since array pointers are given to CWIPI, they are only set
+    // at the first iteration.
+    if (it == itdeb) {
+
+      // Set the mesh :
+      // Here the appropriate CWIPI functions should be called to
+      // set the arrays difining the mesh and finalize it.
+      // ------------------------------------------------------- To fill in
+
+      // ---------------------------------------------------- End To fill in
+
+      // Set fields :
+      // code2 sends the "chinchilla" field and receives "girafe".
+      // ------------------------------------------------------- To fill in
+
+      // ---------------------------------------------------- End To fill in
+
+      // Set interpolation property :
+      CWP_Spatial_interp_property_set(code_name[0],
+                                      coupling_name,
+                                      "tolerance",
+                                      "double",
+                                      "0.1");
+
+    } else {
+
+      // Find the correct CWIPI function to inform CWIPI that the
+      // the time step has been incremented.
+      // ------------------------------------------------------- To fill in
+
+      // ---------------------------------------------------- End To fill in
+    }
+
+    // Compute interpolation weights :
+    CWP_Spatial_interp_weights_compute(code_name[0],
+                                       coupling_name);
+
+    // Exchange
+    CWP_Field_issend(code_name[0],
+                     coupling_name,
+                     send_field_name);
+
+    CWP_Field_irecv(code_name[0],
+                    coupling_name,
+                    recv_field_name);
+
+    CWP_Field_wait_issend(code_name[0],
+                          coupling_name,
+                          send_field_name);
+
+    CWP_Field_wait_irecv(code_name[0],
+                         coupling_name,
+                         recv_field_name);
+
+  // Check interpolation :
+  int n_uncomputed_tgts = CWP_N_uncomputed_tgts_get(code_name[0],
+                                                    coupling_name,
+                                                    recv_field_name,
+                                                    0);
+
+  int *uncomputed_tgts = NULL;
+  if (n_uncomputed_tgts != 0) {
+    uncomputed_tgts = CWP_Uncomputed_tgts_get(code_name[0],
+                                              coupling_name,
+                                              recv_field_name,
+                                              0);
+  }
+
+  PDM_UNUSED(n_uncomputed_tgts);
+  PDM_UNUSED(uncomputed_tgts);
+  } // end interations
+
+  // Delete field :
+  CWP_Field_del(code_name[0],
+                coupling_name,
+                send_field_name);
+
+  CWP_Field_del(code_name[0],
+                coupling_name,
+                recv_field_name);
+
+  // Delete Mesh :
+  CWP_Mesh_interf_del(code_name[0],
+                      coupling_name);
+
+  // Delete the coupling :
+  CWP_Cpl_del(code_name[0],
+              coupling_name);
+
+  // free
+  free(code_name);
+  free(is_active_rank);
+  free(time_init);
+  free(coupled_code_name);
+  free(coords);
+  free(elt_vtx_idx);
+  free(elt_vtx);
+  free(send_field_data);
+  free(recv_field_data);
+
+  // Finalize CWIPI :
+  CWP_Finalize();
+
+  // Finalize MPI :
+  MPI_Finalize();
+
+  return EXIT_SUCCESS;
+}
---
+++ tests/tutorial/fortran_new_api_deformable_ex.F90
@@ -0,0 +1,353 @@
+#include "cwipi_configf.h"
+
+program fortran_new_api_deformable_sol
+
+#ifdef CWP_HAVE_FORTRAN_MPI_MODULE
+    use mpi
+#endif
+    use cwp
+    use pdm_generate_mesh
+
+    implicit none
+
+#ifndef CWP_HAVE_FORTRAN_MPI_MODULE
+    include "mpif.h"
+#endif
+
+  !--------------------------------------------------------------------
+  integer                                     :: ierr
+  integer                                     :: i_rank, n_rank
+
+  integer                                     :: n_code
+  character(len = 99),                pointer :: code_names(:)         => null()
+  integer,                            pointer :: is_coupled_rank(:)    => null()
+  double precision,                   pointer :: time_init(:)          => null()
+  integer,                            pointer :: intra_comms(:)        => null()
+
+  integer                                     :: n_part
+  character(len = 99),                pointer :: coupled_code_names(:) => null()
+  character(len = 99)                         :: coupling_name
+
+  double precision,     dimension(:), pointer :: coords
+  integer(c_long),      dimension(:), pointer :: vtx_g_num => null()
+
+  double precision,     dimension(:), pointer :: xyz_dest
+  integer(c_long),      dimension(:), pointer :: pts_g_num => null()
+
+  integer,                            pointer :: elt_vtx_idx(:)
+  integer,                            pointer :: elt_vtx(:)
+  integer(c_long),      dimension(:), pointer :: elt_g_num => null()
+  integer(c_int)                              :: id_block
+
+  character(len = 99)                         :: send_field_name
+  character(len = 99)                         :: recv_field_name
+  integer(c_int)                              :: n_components
+
+  double precision                            :: ampl
+  double precision                            :: dt
+  double precision                            :: freq
+  double precision                            :: omega
+  double precision                            :: phi
+  double precision                            :: time
+  integer                                     :: i, j, it, itdeb, itend, n2, n_partition
+  integer                                     :: n_vtx, n_elt
+
+  double precision,                   pointer :: send_field_data(:) => null()
+  double precision,                   pointer :: recv_field_data(:) => null()
+
+  integer(c_int)                              :: n_uncomputed_tgts
+  integer(c_int),                     pointer :: uncomputed_tgts(:) => null()
+  !--------------------------------------------------------------------
+
+  ! MPI Initialization :
+  call MPI_Init(ierr)
+  call MPI_Comm_rank(mpi_comm_world, i_rank, ierr)
+  call MPI_Comm_size(mpi_comm_world, n_rank, ierr)
+
+  ! Check running on correct number of MPI ranks :
+  n_partition = 1
+  do while ((2 * n_partition**2) < n_rank)
+    n_partition = n_partition + 1
+  enddo
+
+  n2 = 2 * n_partition**2
+
+  if (n2 /= n_rank) then
+    if (i_rank == 0) then
+      write(6,*) '      Not executed : only available if the number of processus in the form of 2 * n_partition**2'
+    endif
+    call exit()
+  endif
+
+  ! Initialize CWIPI :
+  n_code = 1
+
+  allocate(code_names(n_code),         &
+           is_coupled_rank(n_code),    &
+           time_init(n_code),          &
+           intra_comms(n_code))
+
+  code_names(1)         = "code1"
+  is_coupled_rank(1)    = CWP_STATUS_ON
+  time_init(1)          = 0.d0
+
+  call CWP_Init(mpi_comm_world,  &
+                n_code,          &
+                code_names,      &
+                is_coupled_rank, &
+                time_init,       &
+                intra_comms)
+
+  ! Create the coupling :
+  ! CWP_DYNAMIC_MESH_DEFORMABLE allows us to take into account the modifications
+  ! to the mesh over the coupling steps.
+  coupling_name     = "coupling"
+  allocate(coupled_code_names(n_code))
+  coupled_code_names(1) = "code2"
+  n_part = 1;
+  call CWP_Cpl_create(code_names(1),                                         &
+                      coupling_name,                                         &
+                      coupled_code_names(1),                                 &
+                      CWP_INTERFACE_SURFACE,                                 &
+                      CWP_COMM_PAR_WITH_PART,                                &
+                      CWP_SPATIAL_INTERP_FROM_LOCATION_MESH_LOCATION_OCTREE, &
+                      n_part,                                                &
+                      CWP_DYNAMIC_MESH_DEFORMABLE,                           &
+                      CWP_TIME_EXCH_USER_CONTROLLED)
+
+  ! Set coupling visualisation:
+  call CWP_Visu_set(code_names(1),           &
+                    coupling_name,           &
+                    1,                       &
+                    CWP_VISU_FORMAT_ENSIGHT, &
+                    "text")
+
+  ! Create mesh :
+  call PDM_generate_mesh_rectangle_simplified(intra_comms(1), &
+                                              n_vtx,          &
+                                              n_elt,          &
+                                              coords,         &
+                                              elt_vtx_idx,     &
+                                              elt_vtx)
+
+  ! Interations :
+  ! At each iteration the mesh coordinates and the exchanged fields are modified.
+  itdeb = 1
+  itend = 50
+  freq  = 0.20d0
+  ampl  = 0.012d0
+  phi   = 0.1d0
+  time  = 0.0d0
+  dt    = 0.1d0
+
+  omega = 2.0d0*acos(-1.0d0)*freq
+
+  allocate(send_field_data(n_elt))
+  allocate(recv_field_data(n_elt))
+  allocate(xyz_dest(3 * n_elt))
+  allocate(uncomputed_tgts(n_elt))
+
+  send_field_name = "girafe"
+  recv_field_name = "chinchilla"
+
+  do it = itdeb, itend
+
+    time = (it-itdeb)*dt
+
+    ! Deform mesh :
+
+    do i = 1, n_vtx
+      coords((i-1)*3+3) = (coords((i-1)*3+1)*coords((i-1)*3+1) + coords((i-1)*3+2)*coords((i-1)*3+2))* &
+                          ampl*cos(omega*time+phi)
+    enddo
+
+    ! Update sent field :
+
+    do  i = 1, n_elt
+      send_field_data(i) = 0.
+      do j = elt_vtx_idx(i)+1, elt_vtx_idx(i+1)
+        send_field_data(i) = send_field_data(i) + coords((elt_vtx(j)-1)*3+3)
+      enddo
+      send_field_data(i) = send_field_data(i)/ (elt_vtx_idx(i+1) - elt_vtx_idx(i))
+    enddo
+
+    ! Update user defined degrees of freedom :
+
+    do i = 1, n_elt
+       xyz_dest((i-1)*3+1) = 0.
+       xyz_dest((i-1)*3+2) = 0.
+       xyz_dest((i-1)*3+3) = 0.
+       do j = elt_vtx_idx(i)+1, elt_vtx_idx(i+1)
+           xyz_dest((i-1)*3+1) = xyz_dest((i-1)*3+1) + coords((elt_vtx(j)-1)*3+1)
+           xyz_dest((i-1)*3+2) = xyz_dest((i-1)*3+2) + coords((elt_vtx(j)-1)*3+2)
+           xyz_dest((i-1)*3+3) = xyz_dest((i-1)*3+3) + coords((elt_vtx(j)-1)*3+3)
+       enddo
+       xyz_dest((i-1)*3+1) = xyz_dest((i-1)*3+1) / (elt_vtx_idx(i+1) - elt_vtx_idx(i))
+       xyz_dest((i-1)*3+2) = xyz_dest((i-1)*3+2) / (elt_vtx_idx(i+1) - elt_vtx_idx(i))
+       xyz_dest((i-1)*3+3) = xyz_dest((i-1)*3+3) / (elt_vtx_idx(i+1) - elt_vtx_idx(i))
+    enddo
+
+    if (it == itdeb) then
+
+      ! Set the mesh vertices coordinates :
+      call CWP_Mesh_interf_vtx_set(code_names(1), &
+                                   coupling_name, &
+                                   0,             &
+                                   n_vtx,         &
+                                   coords,        &
+                                   vtx_g_num)
+
+      ! Set the mesh polygons connectivity :
+      id_block = CWP_Mesh_interf_block_add(code_names(1),       &
+                                           coupling_name,       &
+                                           CWP_BLOCK_FACE_POLY)
+
+      call CWP_Mesh_interf_f_poly_block_set(code_names(1), &
+                                            coupling_name, &
+                                            0,             &
+                                            id_block,      &
+                                            n_elt,         &
+                                            elt_vtx_idx,    &
+                                            elt_vtx,        &
+                                            elt_g_num)
+
+      ! When CWP_DOF_LOCATION_USER, calling CWP_User_tgt_pts_set is mandatory :
+      ! This is a point cloud different from the nodes of the mesh that were
+      ! previously set.
+      call CWP_User_tgt_pts_set(code_names(1), &
+                                coupling_name, &
+                                0,             &
+                                n_elt,         &
+                                xyz_dest,      &
+                                pts_g_num)
+
+      ! Finalize mesh :
+      call CWP_Mesh_interf_finalize(code_names(1), &
+                                    coupling_name)
+
+      n_components = 1
+      ! Create field :
+      call CWP_Field_create(code_names(1),                &
+                            coupling_name,                &
+                            send_field_name,              &
+                            CWP_DOUBLE,                   &
+                            CWP_FIELD_STORAGE_INTERLACED, &
+                            n_components,                 &
+                            CWP_DOF_LOCATION_USER,        &
+                            CWP_FIELD_EXCH_SEND,          &
+                            CWP_STATUS_ON)
+
+      ! Set the field values :
+      call CWP_Field_data_set(code_names(1),        &
+                              coupling_name,        &
+                              send_field_name,      &
+                              0,                    &
+                              CWP_FIELD_MAP_SOURCE, &
+                              send_field_data)
+
+      ! Create field :
+      call CWP_Field_create(code_names(1),                &
+                            coupling_name,                &
+                            recv_field_name,              &
+                            CWP_DOUBLE,                   &
+                            CWP_FIELD_STORAGE_INTERLACED, &
+                            n_components,                 &
+                            CWP_DOF_LOCATION_USER,        &
+                            CWP_FIELD_EXCH_RECV,          &
+                            CWP_STATUS_ON)
+
+      ! Set the field values :
+      call CWP_Field_data_set(code_names(1),        &
+                              coupling_name,        &
+                              recv_field_name,      &
+                              0,                    &
+                              CWP_FIELD_MAP_TARGET, &
+                              recv_field_data)
+
+      ! Set interpolation property :
+      call CWP_Spatial_interp_property_set(code_names(1), &
+                                           coupling_name, &
+                                           "tolerance",   &
+                                           "double",      &
+                                           "0.1")
+
+    else
+
+      ! Update time :
+      ! One need to informs CWIPI that we moved to a new interation step.
+      call CWP_Time_update(code_names(1), &
+                           time)
+
+    endif
+
+    ! Compute interpolation weights :
+    call CWP_Spatial_interp_weights_compute(code_names(1), &
+                                            coupling_name)
+
+    ! Exchange field values :
+    call CWP_Field_irecv(code_names(1), &
+                         coupling_name, &
+                         recv_field_name)
+
+    call CWP_Field_issend(code_names(1), &
+                          coupling_name, &
+                          send_field_name)
+
+    call CWP_Field_wait_irecv(code_names(1), &
+                              coupling_name, &
+                              recv_field_name)
+
+    call CWP_Field_wait_issend(code_names(1), &
+                               coupling_name, &
+                               send_field_name)
+
+    ! Check interpolation :
+    n_uncomputed_tgts = CWP_N_uncomputed_tgts_get(code_names(1),   &
+                                                  coupling_name,   &
+                                                  recv_field_name, &
+                                                  0)
+
+    if (n_uncomputed_tgts /= 0) then
+      uncomputed_tgts => CWP_Uncomputed_tgts_get(code_names(1),   &
+                                                 coupling_name,   &
+                                                 recv_field_name, &
+                                                 0)
+    endif
+
+  enddo
+
+  ! Delete field :
+  call CWP_Field_Del(code_names(1),   &
+                     coupling_name,   &
+                     send_field_name)
+
+  call CWP_Field_Del(code_names(1),   &
+                     coupling_name,   &
+                     recv_field_name)
+
+  ! Delete Mesh :
+  call CWP_Mesh_interf_del(code_names(1), &
+                           coupling_name)
+
+  ! Delete the coupling :
+  call CWP_Cpl_Del(code_names(1), &
+                   coupling_name)
+
+  ! free
+  deallocate(code_names)
+  deallocate(is_coupled_rank)
+  deallocate(time_init)
+  deallocate(intra_comms)
+  deallocate(coupled_code_names)
+  deallocate(send_field_data)
+  deallocate(recv_field_data)
+  deallocate(xyz_dest)
+  deallocate(uncomputed_tgts)
+
+  ! Finalize CWIPI :
+  call CWP_Finalize()
+
+  ! Finalize MPI :
+  call MPI_Finalize(ierr)
+
+end program fortran_new_api_deformable_sol
