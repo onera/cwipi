ba28ab1825e16891fa03d5a93fcceaab640eaeda
Karmijn Hoogveld <karmijn.hoogveld@onera.fr>
2023-04-06 10:08:00 +0200

    [tutorial] take into account S. Lala comments

--- src/new/cwp.h
+++ src/new/cwp.h
@@ -636,6 +636,7 @@ CWP_Properties_str_dump
  * \param [in]  local_code_name     Local code name
  * \param [in]  cpl_id              Coupling identifier
  * \param [in]  coupled_code_name   Distant or local coupled code name
+ * \param [in]  entities_dim        Coupling interface type
  * \param [in]  comm_type           Communication type
  * \param [in]  spatial_interp      Spatial interpolation method
  * \param [in]  n_part              Number of interface partition
