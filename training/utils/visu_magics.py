4f5a595c50f581c9c49d35d8231da19e7c6a5bdc
Bastien Andrieu <bastien.andrieu@onera.fr>
2023-10-30 14:04:53 +0100

    [training] add visualize_dynamic

--- training/utils/visu_magics.py
+++ training/utils/visu_magics.py
@@ -16,6 +16,157 @@ def nice_view(plotter):
       plotter.view_xy()
 
 
+def read_ensight(filename):
+  import os
+  import re
+  import numpy as np
+  import pyvista as pv
+  from pyvista import CellType
+
+  match_number = re.compile('\ *-?[0-9]+\.?[0-9]*(?:[Ee]\ *-?\+?\ *[0-9]+)?')
+
+  path = os.path.dirname(filename)
+  if len(path) > 0:
+    if path[-1] != "/":
+      path += "/"
+
+  # read case
+  geom_prefix = None
+  n_steps = -1
+  start   = -1
+  incr    = -1
+  with open(filename, "r") as f:
+    while True:
+      line = f.readline()
+      if line == "": break
+
+      if re.search("GEOMETRY", line) is not None:
+        while True:
+          line = f.readline()
+          if line == "" or line == "\n": break
+          match = re.search("model:", line)
+          if match is not None:
+            info = line[match.span()[1]:].split()
+            idx = info[1].rfind(".")
+            geom_prefix = info[1][:idx]
+            geom_padding = len(info[1]) - idx - 1
+            #geom_mode = info[2]
+            break
+
+      if re.search("VARIABLE", line) is not None:
+        scalars = dict()
+        while True:
+          line = f.readline()
+          if line == "" or line == "\n": break
+          match = re.search("scalar per node:", line)
+          if match is not None:
+            info = line[match.span()[1]:].split()
+            name = info[1]
+            idx = info[2].rfind(".")
+            prefix = info[2][:idx]
+            padding = len(info[2]) - idx - 1
+            scalars[name] = {"prefix": prefix, "padding": padding}
+
+      if re.search("TIME", line) is not None:
+        while True:
+          line = f.readline()
+          if line == "": break
+
+          match = re.search("number of steps:", line)
+          if match is not None:
+            n_steps = int(line[match.span()[1]:])
+            #print(f"n_steps = {n_steps}")
+
+          match = re.search("filename start number:", line)
+          if match is not None:
+            start = int(line[match.span()[1]:])
+            #print(f"start = {start}")
+
+          match = re.search("filename increment:", line)
+          if match is not None:
+            incr = int(line[match.span()[1]:])
+            #print(f"incr = {incr}")
+            break
+
+          # read time values?
+
+  # read geometry
+  instants = dict()
+  #geom_prefix = "chr.geo"
+  for i_step in range(n_steps):
+    fmt = "{:0%d}" % geom_padding
+    geom_name = path + geom_prefix + "." + fmt.format(start + incr*i_step)
+
+    grid = None
+    with open(geom_name, "r") as f:
+      while True:
+        line = f.readline()
+        if line == "": break
+
+        match = re.search("coordinates", line)
+        if match is not None:
+          n_vtx = int(f.readline())
+          points = np.empty((n_vtx, 3), dtype=float)
+          for j in range(3):
+            for i in range(n_vtx):
+              points[i,j] = float(f.readline())
+
+        match = re.search("nsided", line)
+        if match is not None:
+          n_elt = int(f.readline())
+          nside = np.empty(n_elt, dtype=int)
+          size = 0
+          for i in range(n_elt):
+            nside[i] = int(f.readline())
+            size += nside[i] + 1
+
+          connec = np.empty(size, dtype=int)
+          face_vtx = []
+          idx = 0
+          for i in range(n_elt):
+            connec[idx] = nside[i]
+            vertices = [int(a)-1 for a in f.readline().split()]
+            face_vtx.append(vertices)
+            connec[idx+1:idx+1+nside[i]] = vertices
+            idx += nside[i] + 1
+
+          # quick and dirty
+          cell_type = [CellType.POLYGON for i in range(n_elt)]
+          #grid = pv.UnstructuredGrid(connec, cell_type, points)
+
+          grid = {
+            "points": points,
+            "cells" : connec,
+            "face_vtx": face_vtx,
+            "cell_type": cell_type
+          }
+          break
+      instants[i_step] = grid
+
+  # read scalars
+  for name in scalars:
+    padding = scalars[name]["padding"]
+    fmt = "{:0%d}" % padding
+    for i_step in range(n_steps):
+      var_filename = path + scalars[name]["prefix"] + "." + fmt.format(start + incr*i_step)
+      with open(var_filename, "r") as f:
+        while True:
+          line = f.readline()
+          if line == "": break
+
+          match = re.search("coordinates", line)
+          if match is not None:
+            n_vtx = len(instants[i_step]["points"])
+            values = np.empty(n_vtx, dtype=float)
+            for i in range(n_vtx):
+              values[i] = float(f.readline())
+            instants[i_step][name] = values
+  return instants
+
+
+
+
+
 def visu_n_files(files,
                  fields=[""],
                  styles=[""],
@@ -48,6 +199,7 @@ def visu_n_files(files,
     p.enable_anti_aliasing()
 
     for i_file, filename in enumerate(files):
+      # print(filename)
       if not same_view:
         i_col = i_file%n_col
         i_row = i_file//n_col
@@ -55,6 +207,7 @@ def visu_n_files(files,
 
       # load mesh file
       try:
+        # instants = read_ensight(filename)
         mesh = pv.read(filename)
         if not isinstance(mesh, pv.MultiBlock):
           mesh = [mesh]
@@ -98,6 +251,35 @@ def visu_n_files(files,
     print("module not found: pyvista (required for interactive visualization)")
 
 
+def parse_visu_cell(cell):
+  cell_lines = cell.split("\n")
+  n = len(cell_lines)
+  files  = []
+  fields = []
+  styles = []
+
+  for l in cell_lines:
+    a = [k.rstrip().lstrip() for k in l.split(":")]
+
+    if len(a[0]) > 0:
+      files.append(a[0])
+
+      if len(a) > 1:
+        fields.append(a[1])
+      else:
+        fields.append("")
+
+      if len(a) > 2:
+        styles.append(a[2])
+      else:
+        styles.append("")
+  return {
+    "files" : files,
+    "fields": fields,
+    "styles": styles
+  }
+
+
 @magics_class
 class VisuMagics(Magics):
   """
@@ -115,11 +297,7 @@ class VisuMagics(Magics):
     args = magic_arguments.parse_argstring(self.visualize, line)
 
     # parse cell content
-    cell_lines = cell.split("\n")
-    n = len(cell_lines)
-    files  = []
-    fields = []
-    styles = []
+    params = parse_visu_cell(cell)
 
     for l in cell_lines:
       a = [k.rstrip().lstrip() for k in l.split(":")]
@@ -137,11 +315,165 @@ class VisuMagics(Magics):
         else:
           styles.append("")
 
-    visu_n_files(files     = files,
-                 fields    = fields,
-                 styles    = styles,
+    visu_n_files(files     = params["files"],
+                 fields    = params["fields"],
+                 styles    = params["styles"],
                  same_view = args.same_view,
                  lighting  = not args.no_lighting)
+
+
+  """
+  Visualize dynamic data sets
+  """
+  @cell_magic
+  @magic_arguments.magic_arguments()
+  @magic_arguments.argument('--same_view', '-sv',
+                            help='Show all files in same view',
+                            action="store_true")
+  def visualize_dynamic(self, line='', cell=None):
+    args = magic_arguments.parse_argstring(self.visualize, line)
+
+    # parse cell content
+    params = parse_visu_cell(cell)
+
+    """
+    Interactive visu (TODO: move to separate function)
+    """
+    from ipywidgets import interactive, IntSlider, HBox
+    import matplotlib.pyplot as plt
+    from matplotlib.colors import Colormap
+    import numpy as np
+    import copy
+
+    # read data sets
+    datasets = dict()
+    for name in params["files"]:
+      datasets[name] = read_ensight(name)
+
+    n_files = len(params["files"])
+    same_view = args.same_view or (n_files < 2)
+
+    # get number of time steps
+    n_steps = 0
+    for name in datasets:
+      n_steps = max(n_steps, len(datasets[name]))
+
+    # compute global bbox over all time steps
+    xymin =  1e30 * np.ones(2)
+    xymax = -1e30 * np.ones(2)
+    for name in datasets:
+      for i_step in datasets[name]:
+        coords = datasets[name][i_step]["points"]
+        xymin = np.minimum(xymin, np.amin(coords[:,:2], axis=0))
+        xymax = np.maximum(xymax, np.amax(coords[:,:2], axis=0))
+    xyeps = 1e-2 * np.amax(xymax - xymin)
+    xymin -= xyeps
+    xymax += xyeps
+    xmin, ymin = xymin
+    xmax, ymax = xymax
+
+    # compute scalar extrema over all time steps
+    for i_dataset in range(n_files):
+      name = params["files" ][i_dataset]
+      vmin =  1e30
+      vmax = -1e30
+      field = params["fields"][i_dataset]
+      for i_step in datasets[name]:
+        if field in datasets[name][i_step]:
+          values = datasets[name][i_step][field]
+          vmin = min(vmin, np.amin(values))
+          _vmax = np.amax(values)
+          if _vmax < 1e6:
+            vmax = max(vmax, _vmax)
+      datasets[name]["vmin"] = vmin
+      datasets[name]["vmax"] = vmax
+
+    # TODO: link extrema?
+
+    def visu(step):
+      figsize = (15, 10)
+      dpi = 72
+
+      if same_view:
+        fig, ax = plt.subplots(figsize=figsize, dpi=dpi)
+      else:
+        n_col = 2
+        n_row = n_files//n_col
+        fig, axes = plt.subplots(n_row, n_col, figsize=figsize, dpi=dpi)
+
+      for i_dataset in range(n_files):
+        name  = params["files" ][i_dataset]
+        field = params["fields"][i_dataset]
+        style = params["styles"][i_dataset]
+
+        coords = datasets[name][step]["points"]
+        connec = datasets[name][step]["face_vtx"]
+        if field in datasets[name][step]:
+          values = datasets[name][step][field]
+        else:
+          values = None
+
+        if not same_view:
+          i_col = i_dataset%n_col
+          i_row = i_dataset//n_col
+          if n_row == 1:
+            ax = axes[i_col]
+          else:
+            ax = axes[i_row, i_col]
+
+        if values is None:
+          color = "0.5"
+          lw    = 1.4
+        else:
+          #cmap = Colormap("coolwarm")
+          cmap = copy.copy(plt.cm.get_cmap('coolwarm'))
+          cmap.set_over("w")#'0.5')
+          if style == "points":
+            """ax.scatter(coords[:,0], coords[:,1],
+                       c=values,
+                       cmap=cmap,
+                       zorder=3)"""
+            status_name = field[:-1] + "_status"
+            if status_name in datasets[name][step]:
+              status = datasets[name][step][status_name]
+              mapped   = np.where(np.isclose(status, 0))
+              unmapped = np.where(np.isclose(status, 1))
+              ax.plot(coords[mapped,  0], coords[mapped,  1], "ko", mfc="k", ms=5, zorder=3)
+              ax.plot(coords[unmapped,0], coords[unmapped,1], "ko", mfc="w", ms=5, zorder=3)
+
+          tpc = ax.tripcolor(coords[:,0], coords[:,1], connec,
+                             values,
+                             shading="gouraud",
+                             cmap=cmap,
+                             vmin=datasets[name]["vmin"],
+                             vmax=datasets[name]["vmax"],
+                             zorder=1)
+
+          cbar = fig.colorbar(tpc,
+                              ax=ax,
+                              orientation="horizontal")
+          # cbar.ax.tick_params(labelsize=14)
+          cbar.set_label(label=field, size=14)
+          color = "k"
+          lw    = 0.7
+
+        ax.triplot(coords[:,0], coords[:,1], connec, "-", color=color, lw=lw, zorder=2)
+
+        ax.set_aspect("equal")
+        ax.set_xlim(xmin, xmax)
+        ax.set_ylim(ymin, ymax)
+        ax.axis("off")
+
+      plt.show()
+
+    step = IntSlider(min=0, max=n_steps-1, step=1, value=0, continuous_update=True)
+    ui = HBox([step])
+
+    interactive_plot = interactive(visu, step=step)
+    out = interactive_plot.children[-1]
+    out.layout.height = '500px'
+    display(ui, out)
+
 
 
 def load_ipython_extension(ipython):
