34d24f1fa788adb5d4d2084afc3cacefdabb1771
Bastien Andrieu <bastien.andrieu@onera.fr>
2024-02-27 15:20:53 +0100

    [mesh] fix shadowed declarations

--- src/new/mesh.cxx
+++ src/new/mesh.cxx
@@ -594,7 +594,7 @@ namespace cwipi {
 
     if (_faceEdgeMethod == 1) {
 
-      int compute_gnum = 0;
+      compute_gnum = 0;
       for (int i_part = 0; i_part < _npart; i_part++) {
         if (_faceLNToGN[i_part] == NULL && _nFace[i_part] > 0) {
           compute_gnum = 1;
@@ -602,7 +602,7 @@ namespace cwipi {
         }
       }
 
-      int _compute_gnum = compute_gnum;
+      _compute_gnum = compute_gnum;
       PDM_MPI_Allreduce (&_compute_gnum, &compute_gnum, 1, PDM_MPI_INT, PDM_MPI_MAX,
                          _pdm_localComm);
 
@@ -641,7 +641,7 @@ namespace cwipi {
 
     else if (_faceVtxMethod == 1) {
 
-      int compute_gnum = 0;
+      compute_gnum = 0;
       for (int i_part = 0; i_part < _npart; i_part++) {
         if (_faceLNToGN[i_part] == NULL && _nFace[i_part] > 0) {
           compute_gnum = 1;
@@ -649,7 +649,7 @@ namespace cwipi {
         }
       }
 
-      int _compute_gnum = compute_gnum;
+      _compute_gnum = compute_gnum;
       PDM_MPI_Allreduce (&_compute_gnum, &compute_gnum, 1, PDM_MPI_INT, PDM_MPI_MAX,
                          _pdm_localComm);
 
@@ -685,11 +685,15 @@ namespace cwipi {
 
       int compute_face_gnum = 0;
       for (int i_part = 0; i_part < _npart; i_part++) {
-        if (_faceLNToGN[i_part] == NULL && _nCells[i_part] > 0) {
-          compute_face_gnum = 1;
+        if (_faceLNToGN[i_part] == NULL && _nFace[i_part] > 0) {
+          compute_gnum = 1;
           break;
         }
       }
+
+      int _compute_face_gnum = compute_face_gnum;
+      PDM_MPI_Allreduce (&_compute_face_gnum, &compute_face_gnum, 1, PDM_MPI_INT, PDM_MPI_MAX,
+                         _pdm_localComm);
 
       if (compute_face_gnum) {
 
@@ -708,7 +712,7 @@ namespace cwipi {
       }
 
 
-      int compute_gnum = 0;
+      compute_gnum = 0;
       for (int i_part = 0; i_part < _npart; i_part++) {
         if (_cellLNToGN[i_part] == NULL && _nCells[i_part] > 0) {
           compute_gnum = 1;
@@ -716,7 +720,7 @@ namespace cwipi {
         }
       }
 
-      int _compute_gnum = compute_gnum;
+      _compute_gnum = compute_gnum;
       PDM_MPI_Allreduce (&_compute_gnum, &compute_gnum, 1, PDM_MPI_INT, PDM_MPI_MAX,
                          _pdm_localComm);
 
@@ -841,7 +845,7 @@ namespace cwipi {
         }
       }
 
-      int compute_gnum = 0;
+      compute_gnum = 0;
 
       for(int i_part = 0; i_part < _npart; i_part++){
 
@@ -854,7 +858,7 @@ namespace cwipi {
         }
       }
 
-      int _compute_gnum = compute_gnum;
+      _compute_gnum = compute_gnum;
       PDM_MPI_Allreduce (&_compute_gnum, &compute_gnum, 1, PDM_MPI_INT, PDM_MPI_MAX,
                          _pdm_localComm);
 
